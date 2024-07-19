45651fa59f4b24e71f08e6a33698e6852559dc14
le-lien6723 <le_lien@bs-ja.co.jp>
2024-07-19 16:42:03 +0900

    Configure DB & Redis & add migration bundle

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ if(builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
-    builder.Services.AddDistributedMemoryCache();
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+        options.InstanceName = "SampleInstance";
+    });
 }
 else
 {
